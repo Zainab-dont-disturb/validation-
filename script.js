14db0bb11e007586c4f0550ef499ab9c725a6be8
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-11-19 17:04:51 +0000

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+function submit(){
+var name=document.getElementById("name").value;
+var age=document.getElementById("age").value;
+if (name==""){
+document.getElementById("nameerror").innerHTML="name is empty";
+
+}
+if (age==""){
+    document.getElementById("ageerror").innerHTML="age is empty";
+    
+    }
+    
+
+
+}
